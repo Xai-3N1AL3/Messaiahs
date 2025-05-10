ae49d7cfadf87ee114f3707f44e8f39f4b628688
sheninna <sheninnajexellcaisip@gmail.com>
2025-05-11 00:42:06 +0800

    not yet done

---
+++ messaiah/src/components/layout/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import './Sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar bg-warning">
+      <div className="p-3">
+        <h1 className="logo">☕ Messiah's Admin</h1>
+        <nav>
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">📋 Dashboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/settings" className="nav-link">⚙️ Settings</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/menu" className="nav-link">🍽️ Menu Management</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/salesreport" className="nav-link">📊 Sales Reports</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
